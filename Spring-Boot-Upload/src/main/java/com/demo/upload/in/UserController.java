a92af4ca059cd1e211de4fd2d374883b30888b1d
Castulo Ramirez londono <VYG0769@uniqa.at>
2021-10-29 18:58:22 +0200

    FIXED. Now It Runs

--- Spring-Boot-Upload/src/main/java/com/demo/upload/in/UserController.java
+++ Spring-Boot-Upload/src/main/java/com/demo/upload/in/UserController.java
@@ -1,9 +1,11 @@
 package com.demo.upload.in;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,8 +19,10 @@ public class UserController {
 	@PostMapping(value = "/upload", consumes = { MediaType.APPLICATION_JSON_VALUE,
 												 MediaType.MULTIPART_FORM_DATA_VALUE })
 
-	public User upload(@RequestPart("user") String user, @RequestPart("file") List<MultipartFile> file) {
+	public User upload(@RequestParam("user") String user, @RequestPart("file") List<MultipartFile> file) {
 
+		System.out.println("UserController.upload()"
+				+user);
 		User userJson = userService.getJson(user, file);
 		return userJson;
 	}
